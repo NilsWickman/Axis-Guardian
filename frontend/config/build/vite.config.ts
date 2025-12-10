8da66f84947d1ec967bfd3a57dae89c006dbddff
NilsWickman <wickman.nils@gmail.com>
2025-12-10 15:26:15 +0100

    Add polyfill

--- frontend/config/build/vite.config.ts
+++ frontend/config/build/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      include: ['buffer'],
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
   define: {
     // Polyfill for msgpack-lite which uses Node's Buffer
     global: 'globalThis',
